99efa8c40092bd1fb20658af79d09ca052862108
André Soares <andreborbapsoares@gmail.com>
2026-05-15 22:17:28 -0300

    modularizacao
    separei a main das functions

---
+++ func_calc.py
@@ -0,0 +1,41 @@
+def simular_botoes(x):
+   
+   ### A interface final vai lidar com botões. Por enquanto usaremos inputs numericos para os testes.
+   # 0 a 9 = numeros
+   # 10 = "="
+   # 11 = "+"
+   # 12 = "-"
+   # 13 = "x"
+   # 14 = "/"
+   # 15 = apagar
+   iu = int(input("Digite: "))
+   if iu >= 10 or iu < 0:
+    if iu == 10:
+       return 
+    else: 
+        if iu == 11:
+           return 
+        else:
+            if iu == 12:
+               return 
+            else:
+                if iu == 13:
+                   return 
+                else:
+                    if iu == 14:
+                       return 
+                    else:
+                        if iu == 15:
+                           if not x:
+                              return x
+                           else:
+                              x.pop()
+                              return x
+                        else:
+                            global ligado 
+                            ligado = False
+                            return 
+        
+   else:
+      x.append(iu)
+      return x
--- main.py
+++ main.py
@@ -1,44 +1,7 @@
+from func_calc import simular_botoes
 editor = []
 ligado = True
 
-def simular_botoes():
-   
-   ### A interface final vai lidar com botões. Por enquanto usaremos inputs numericos para os testes.
-   # 0 a 9 = numeros
-   # 10 = "="
-   # 11 = "+"
-   # 12 = "-"
-   # 13 = "x"
-   # 14 = "/"
-   # 15 = apagar
-
-   iu = int(input("Digite: "))
-   if iu >= 10:
-    if iu == 10:
-       return 
-    else: 
-        if iu == 11:
-           return 
-        else:
-            if iu == 12:
-               return 
-            else:
-                if iu == 13:
-                   return 
-                else:
-                    if iu == 14:
-                       return 
-                    else:
-                        if iu == 15:
-                           return simular_botoes.pop()
-                        else:
-                            global ligado 
-                            ligado = False
-                            return 
-        
-   else:
-      return simular_botoes.append(iu)
-
 while ligado == True:
-   simular_botoes()
-   print(simular_botoes)
+   editor = simular_botoes(editor)
+   print(editor)
